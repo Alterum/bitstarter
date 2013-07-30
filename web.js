7023ff1a8aee72e75081622b051253e7fbe0500b
Jawi <jk.mtnv@yahoo.com>
2013-07-30 18:31:47 +0000

    modify web.js: added index.html content

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World and Jawi too!');
+  var buffer = new Buffer(32);
+  response.send(
+	fs.readFileSync('index.html').toString('utf-8'));
+  
+//  response.send('Hello World and Jawi too!');
 });
 
 var port = process.env.PORT || 5000;
